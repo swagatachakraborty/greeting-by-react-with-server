f188999acb01a44b6b9ece6aecb0d5ecb317007f
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-04-16 15:21:08 +0530

    Remove build from gitignore.

---
+++ client/build/precache-manifest.464b9bf825d302d22c5397d0d4585da4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d9b4e4f73f5445c80ee9",
+    "url": "/static/js/main.5a9dd6a3.chunk.js"
+  },
+  {
+    "revision": "6cf7e1e38fce15dd1096",
+    "url": "/static/js/2.7517a49d.chunk.js"
+  },
+  {
+    "revision": "d9b4e4f73f5445c80ee9",
+    "url": "/static/css/main.7b12dd5e.chunk.css"
+  },
+  {
+    "revision": "333e2445d57c6fa8ff1c06c28b994cce",
+    "url": "/index.html"
+  }
+];
